432bdb0881f4824fd4093ad0191e8e6e8b573d03
Julia Vecchione <juliavecchione12@gmail.com>
2024-04-06 17:01:48 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let menuIcon = document.querySelector("#meni-icon");
+let nev = document.querySelector(".nevbar");
+let section = document.querySelector("section");
+let navLinks = document.querySelector("header nav a");
